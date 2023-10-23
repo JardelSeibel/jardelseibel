1e604720be9de3d265e375931c5522292aa65b2f
JardelSeibel <146040337+JardelSeibel@users.noreply.github.com>
2023-10-23 17:43:56 -0500

    Add files via upload

--- Composteira.py
+++ Composteira.py
@@ -1,35 +1,39 @@
+import adafruit_ads1x15.ads1115 as ADS
 import paho.mqtt.client as mqtt
 import Adafruit_DHT as dht
 import RPi.GPIO as GPIO
+import board
+import busio
 import time
 import json
 import glob
 import sys
 import os
+from adafruit_ads1x15.analog_in import AnalogIn
 
+### Informações para conexão com a plataforma ThingsBoard ###
 THINGSBOARD_HOST = 'demo.thingsboard.io'
 ACCESS_TOKEN = '4JlWXm7ZW3nQommCSsQk'
 
-
-# Data capture and upload interval in seconds. Less interval will eventually hang the DHT22.
-INTERVAL = 3
-
+### Definição das tags para envio ao ThingsBoard ###
 sensor_data = {'temperature': 0, 'humidity': 0}
 sensor_temp = {'temperature2': 0}
+sensor_umid = {'umidade': 0}
+sensor_ph = {'ph': 0}
 
-next_reading = time.time() 
+next_reading = time.time()
 
+### Conexão via protocolo MQTT ###
 client = mqtt.Client()
 
-# Set access token
+### Token de Acesso ###
 client.username_pw_set(ACCESS_TOKEN)
 
-# Connect to ThingsBoard using default MQTT port and 60 seconds keepalive interval
+### Conexão ao ThingsBoard usando o protocolo MQTT em um intervalo de 60s ###
 client.connect(THINGSBOARD_HOST, 1883, 60)
-
 client.loop_start()
 
-########## sensor temp 2 ######
+### Leitura do sensor de temperatura DS18B20 ###
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
  
@@ -46,33 +50,66 @@ def read_temp_raw():
 def read_temp():
     lines = read_temp_raw()
     while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
         lines = read_temp_raw()
     equals_pos = lines[1].find('t=')
     if equals_pos != -1:
         temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
         return (temp_c)
-##########
+    
+### Leitura do barramento i2c e definição dos canais ###
+i2c = busio.I2C(board.SCL, board.SDA)
+ads = ADS.ADS1115(i2c)
+canal0 = 0
+canal1 = 1
+
+### Cálculo umidade do solo ###
+def calcular_umidade(valor1):
+    valor_min = 26372
+    valor_max = 4000
+    umidade_min = 0
+    umidade_max = 100
+    umidade = ((valor1 - valor_min) / (valor_max - valor_min)) * (umidade_max - umidade_min) + umidade_min
+    return umidade
+
+### Cálculo pH do solo ###
+def calcular_ph(valor2):
+    calculo_tensao = (valor2/65535.0) * 5.0
+    ph = ((calculo_tensao-0) * 1.2) + 3
+    return ph
+
 try:
     while True:
         humidity,temperature = dht.read_retry(dht.DHT22, 27)
         temperature2 = read_temp()
+        chan1 = AnalogIn(ads, canal0)
+        valor1 = chan1.value
+        chan2 = AnalogIn(ads, canal1)
+        valor2 = chan2.value
+        umidade = calcular_umidade(valor1)
+        ph = calcular_ph(valor2)
         humidity = round(humidity, 2)
         temperature = round(temperature, 2)
-        print(u"Temperature: {:g}\u00b0C, Humidity: {:g}%, Temperature2: {:g}\u00b0C".format(temperature, humidity, temperature2))
+        print("Temperatura DHT22: {:g}\u00b0C, Umidade DHT22: {:.2f} %".format(temperature, humidity))
+        print("Temperatura DS18B20: {:g}\u00b0C".format(temperature2))
+        print("Umidade do solo: {:.2f} %".format(umidade))
+        print("pH do solo: {:.2f}".format(ph))
+        print("Aguardando 5 segundos para a próxima leitura")
+        print("--------------------------------------------")
         sensor_data['temperature'] = temperature
         sensor_data['humidity'] = humidity
         sensor_temp['temperature2'] = temperature2
-
-        # Sending humidity and temperature data to ThingsBoard
+        sensor_umid['umidade'] = umidade
+        sensor_ph['ph'] = ph
+        
+        # Envio dos sinais de monitoramento para o ThingsBoard
         client.publish('v1/devices/me/telemetry', json.dumps(sensor_data), 1)
         client.publish('v1/devices/me/telemetry', json.dumps(sensor_temp), 1)
+        client.publish('v1/devices/me/telemetry', json.dumps(sensor_umid), 1)
+        client.publish('v1/devices/me/telemetry', json.dumps(sensor_ph), 1)
         
-        next_reading += INTERVAL
-        sleep_time = next_reading-time.time()
-        if sleep_time > 0:
-            time.sleep(sleep_time)
+        time.sleep(5)
+        
 except KeyboardInterrupt:
     pass
 
